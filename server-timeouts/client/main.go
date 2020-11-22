5d47a4101bf89a9c7914e9958e04e1558e3890ef
Tugberk Ugurlu <tugberk@outlook.com>
2020-11-22 10:06:11 +0000

    check server timeout behaviour
    in [here](https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/), it states:
    
    > WriteTimeout normally covers the time from the end of the request header read to the end of the response write (a.k.a. the lifetime of the ServeHTTP), by calling SetWriteDeadline at the end of readRequest.
    
    this is actually the behaviour, and the response errors on the client
    side when WriteTimeout is reached. That said, we still wait for the amount of time that it takes to process the request, regardless of the write timeout value. However, when it comes to writing the response, the server closes the conn w/o actually attempting to write it.

---
+++ server-timeouts/client/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type connWrapper struct {
+	net.Conn
+}
+
+func (ct *connWrapper) Close() error {
+	err := ct.Conn.Close()
+	if err != nil {
+		fmt.Println("conn close failed", ct.Conn.LocalAddr(), ct.Conn.RemoteAddr())
+	} else {
+		fmt.Println("conn closed", ct.Conn.LocalAddr(), ct.Conn.RemoteAddr())
+	}
+	return err
+}
+
+func main() {
+	t := http.DefaultTransport.(*http.Transport).Clone()
+	dialer := net.Dialer{}
+	t.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		conn, err := dialer.DialContext(ctx, network, addr)
+		if err != nil {
+			return conn, err
+		}
+		fmt.Println("conn established", conn.LocalAddr(), conn.RemoteAddr())
+		return &connWrapper{
+			conn,
+		}, nil
+	}
+	c := http.Client{
+		Timeout:   30 * time.Second, // set high for the purposes of our test
+		Transport: t,
+	}
+
+	cc := make(chan os.Signal)
+	signal.Notify(cc, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-cc
+		fmt.Println("\r- Ctrl+C pressed in Terminal")
+		c.CloseIdleConnections()
+		os.Exit(0)
+	}()
+	func() {
+		r, err := c.Get("http://localhost:4400")
+		defer func() {
+			if r != nil && r.Body != nil {
+				r.Body.Close()
+			}
+		}()
+		if err != nil {
+			log.Printf("HTTP Get err: %v\n", err)
+			return
+		}
+		val, _ := ioutil.ReadAll(r.Body)
+		log.Println(val)
+	}()
+	select {}
+}
---
+++ server-timeouts/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+const (
+	fakeRequestProcessingTime = 10 * time.Second
+	readTimeout               = fakeRequestProcessingTime / 5
+	writeTimeout              = fakeRequestProcessingTime / 2
+)
+
+type handler struct {
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t := time.NewTimer(fakeRequestProcessingTime) // simulate a long running process
+	defer t.Stop()
+
+	select {
+	case <-r.Context().Done():
+		log.Println("stopped processing")
+	case <-t.C:
+		log.Println("writing response", r.Proto)
+		_, err := w.Write([]byte("hello world"))
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func main() {
+	srv := http.Server{
+		Addr:         ":4400",
+		Handler:      handler{},
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		ConnState: func(conn net.Conn, state http.ConnState) {
+			log.Printf("[%s,%s]: %s", conn.RemoteAddr().String(),
+				conn.LocalAddr().String(),
+				state.String())
+		},
+	}
+
+	err := srv.ListenAndServe()
+	log.Println(err)
+}
